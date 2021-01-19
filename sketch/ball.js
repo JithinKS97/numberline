e05e36ae3fd7cbe60c440406b9dab01af690e090
Admin <jithinks112@gmail.com>
2021-01-19 23:58:41 +0530

    added jump

--- sketch/ball.js
+++ sketch/ball.js
@@ -1,19 +1,23 @@
-const noOfSteps = 15
+const noOfSteps = 25
 
 class Ball {
     constructor({ x, y }) {
         this.x = x
         this.y = y
         this.distToTravel = 0;
+
+        this.curveCenter = createVector()
+        this.yJump = 0
     }
 
     display() {
         fill(220, 0, 150)
-        circle(this.x, this.y, 15)
+        circle(this.x, this.y + this.yJump, 15)
         this.move();
     }
 
     goToPix(target) {
+        this.curveCenter = createVector(this.x + (target - this.x)/2, this.y)
         this.resetSteps();
         this.distToTravel = target - this.x;
         this.step = this.distToTravel/this.noOfSteps
@@ -24,6 +28,9 @@ class Ball {
             this.step = this.distToTravel/noOfSteps
             this.x+=this.step;
             this.noOfSteps--;
+
+            this.yMap = map(this.noOfSteps, 0, noOfSteps, 0, PI)
+            this.yJump = -25*sin(this.yMap)
         }
     }
 
